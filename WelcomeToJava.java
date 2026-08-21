bdd327b3c858b93ac2d3a3764c21112e81f0bce3
shreyas patel <srshreyaspatel@gmail.com>
2026-08-21 12:12:12 +0530

    in_oneline

--- WelcomeToJava.java
+++ WelcomeToJava.java
@@ -3,6 +3,11 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         System.out.println("welcome");
+        System.out.println("to");
+        System.out.println("java");
+        System.out.print("welcome ");
+        System.out.print("to ");
+        System.out.print("java ");
 
     }
 }
