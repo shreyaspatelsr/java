9913ef1ad181905819dabd852fa5b74de8ef3452
shreyas patel <srshreyaspatel@gmail.com>
2026-08-30 23:08:41 +0530

    if else

---
+++ M2/topics/subtopics/if_else_types/Prgm1.java
@@ -0,0 +1,12 @@
+
+public class Prgm1 {
+
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 99) {
+            System.out.println("it is greater");
+        } else {
+            System.out.println("num is not greater");
+        }
+    }
+}
