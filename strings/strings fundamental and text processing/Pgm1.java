a94a988b0b09c4af90688b33bfcfcf527567ab47
shreyas patel <srshreyaspatel@gmail.com>
2026-09-09 19:46:43 +0530

    string concatination

---
+++ strings/strings fundamental and text processing/Pgm1.java
@@ -0,0 +1,16 @@
+
+class Pgm1 {
+
+    public static void main(String[] args) {
+        String s1 = "raja";
+        String s2 = "rani";
+        String s3 = "raja" + "rani";
+        System.out.println(s3);
+        String s4 = new String("anu");
+        String s5 = new String("banu");
+        String res = s4.concat(s5);
+        System.out.println(res);
+
+    }
+
+}
