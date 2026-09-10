dec3b461589da8e1bd0d7ad10a34db840541fbca
shreyas patel <srshreyaspatel@gmail.com>
2026-09-10 22:30:45 +0530

    allocate capacity

---
+++ strings/string builder/Pgm2.java
@@ -0,0 +1,15 @@
+
+public class Pgm2 {
+// allocating capacity
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("java");
+        System.out.println(sb1);
+        sb1.append("program");
+        System.out.println(sb1);
+        StringBuilder sb2 = new StringBuilder();
+        sb2.ensureCapacity(100);
+        System.out.println(sb2.capacity());
+
+    }
+}
