55eb773388bc6893012ae7cbb8c94f17f358099e
shreyas patel <srshreyaspatel@gmail.com>
2026-09-10 19:35:03 +0530

    insert delete reverse

---
+++ strings/string builder/Pgm3.java
@@ -0,0 +1,17 @@
+
+public class Pgm3 {
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("java");
+        System.out.println(sb1);
+        sb1.append("program");
+        System.out.println(sb1);
+        sb1.insert(0, "python");
+        System.out.println(sb1);
+        sb1.delete(0, 4);
+        System.out.println(sb1);
+        sb1.reverse();
+        System.out.println(sb1);
+
+    }
+}
