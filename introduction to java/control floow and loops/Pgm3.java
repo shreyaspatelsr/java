de5d6536b7d445d28d0629c12c117c74b6de18dc
shreyas patel <srshreyaspatel@gmail.com>
2026-09-12 15:38:18 +0530

    post increment and pre increment

---
+++ introduction to java/control floow and loops/Pgm3.java
@@ -0,0 +1,14 @@
+
+public class Pgm3 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = a++;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        int c = 40;
+        int d = ++c;
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+}
