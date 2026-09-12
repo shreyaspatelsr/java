9c8bc5de26e4245c953eb03cc2a64a393396b568
shreyas patel <srshreyaspatel@gmail.com>
2026-09-12 16:54:01 +0530

    NestedLoopfor

---
+++ introduction to java/control floow and loops/NestedLoop1.java
@@ -0,0 +1,13 @@
+
+public class NestedLoop1 {
+
+    // nested for loop
+    public static void main(String[] args) {
+        for (int j = 1; j <= 5; j++) {
+            for (int i = 1; i <= 20; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
