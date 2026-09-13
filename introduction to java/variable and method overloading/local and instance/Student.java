d116fcd84ed47e81b0cf5655888c47f301a96f79
shreyas patel <srshreyaspatel@gmail.com>
2026-09-13 23:21:49 +0530

    shadowing

---
+++ introduction to java/variable and method overloading/local and instance/Student.java
@@ -0,0 +1,11 @@
+
+class Student {
+
+    int id;
+    String name;
+
+    void display() {
+        int a = 10;
+        System.out.println(a);
+    }
+}
---
+++ introduction to java/variable and method overloading/local and instance/StudentApp.java
@@ -0,0 +1,10 @@
+
+class StudentApp {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+        s1.display();
+    }
+}
