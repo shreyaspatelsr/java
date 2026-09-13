08c094b5c9a831d16946f5d5fc214f964ed1d4f6
shreyas patel <srshreyaspatel@gmail.com>
2026-09-13 19:50:29 +0530

    methods types

--- introduction to java/Classes objects and methods/method types/Type4.java
+++ introduction to java/Classes objects and methods/method types/Type4.java
@@ -2,13 +2,14 @@
 public class Type4 {
 
     public static void main(String[] args) {
-        add(10, 20); // arguments
+        int c = add(10, 20); // arguments
+        System.out.println(c);
     }
 
     // with parameters and with return type
     static int add(int a, int b) {
         int c = a + b;
-        System.out.println(c);
+
         return c;
     }
 }
